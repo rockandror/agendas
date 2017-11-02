2cc082f179ff67458b333cd6aa8b7dcbd162b8c4
Senén Rodero Rodríguez <senenrodero@gmail.com>
2017-11-02 13:21:25 +0100

    Fix broken spec on travis

--- app/models/holder.rb
+++ app/models/holder.rb
@@ -1,3 +1,4 @@
+
 class Holder < ActiveRecord::Base
 
   # Relations
--- spec/features/users/user_delete_spec.rb
+++ spec/features/users/user_delete_spec.rb
@@ -1,14 +1,14 @@
 feature 'User delete', :devise, :js do
 
-  scenario 'user can not delete own account', :js do
+  scenario 'user can not delete own account' do
     user = create(:user, :admin)
     login_as(user, :scope => :user)
 
     visit users_path
 
-    expect(page).to have_content I18n.t 'backend.users'
-    save_screenshot
-    click_link('', href: user_path(user))
+    within "#user_#{user.id}" do
+      find('a.delete').click
+    end
     expect(page).to have_content 'No ha sido posible realizar la operación'
   end
 
